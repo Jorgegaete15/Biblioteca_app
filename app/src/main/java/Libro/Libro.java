1de03eaf1cfa407b8525d53615bf85fd295d7b22
jorgegaete15 <j.gaete8@alumnos.santotomas.cl>
2020-11-18 18:35:45 -0300

    actualizacion de libros

--- app/src/main/java/Libro/Libro.java
+++ app/src/main/java/Libro/Libro.java
@@ -1,26 +1,30 @@
 package Libro;
-
 public class Libro {
-
     private int Farenheit;
     private int Revival;
     private int ElAlquimista;
-
+    private int ElPoder;
+    private int Despertar;
     public Libro() {
-        Farenheit = 5000;
-        Revival = 12000;
+        Farenheit = 7000;
+        Revival = 22000;
         ElAlquimista = 45000;
+        ElPoder = 88000;
+        Despertar = 156000;
     }
-
     public int getFarenheit() {
         return Farenheit;
     }
-
     public int getRevival() {
         return Revival;
     }
-
     public int getAlquimista() {
         return ElAlquimista;
+    }
+    public int getElPoder() {
+        return ElPoder;
+    }
+    public int getDespertar() {
+        return Despertar;
     }
 }
